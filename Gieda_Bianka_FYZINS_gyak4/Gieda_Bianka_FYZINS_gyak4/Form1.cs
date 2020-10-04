fecc1164705881f2d9137659c388a239f27448ec
Bianka Gieda <bianka.gieda@stud.uni-corvinus.hu>
2020-10-04 21:06:53 +0200

    6. feladat

--- Gieda_Bianka_FYZINS_gyak4/Gieda_Bianka_FYZINS_gyak4/Form1.cs
+++ Gieda_Bianka_FYZINS_gyak4/Gieda_Bianka_FYZINS_gyak4/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace Gieda_Bianka_FYZINS_gyak4
 {
@@ -14,14 +16,47 @@ namespace Gieda_Bianka_FYZINS_gyak4
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
+
         private void LoadData()
         {
             Flats = context.Flats.ToList();
         }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
     }
 }
