6d1323155733c05e9f10649f37abe786c5f14a9c
Bianka Gieda <bianka.gieda@stud.uni-corvinus.hu>
2020-10-25 20:13:28 +0100

    3. feladat - Osztályok létrehozása

---
+++ Gieda_Bianka_FYZINS_gyak7/Gieda_Bianka_FYZINS_gyak7/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gieda_Bianka_FYZINS_gyak7.Entities
+{
+    class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public double ProbabilityOfDeath { get; set; }
+    }
+}
