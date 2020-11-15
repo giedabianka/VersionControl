50b9d811afa7f6ce2767b016bbaf36a5214296cd
Bianka Gieda <bianka.gieda@stud.uni-corvinus.hu>
2020-11-15 16:21:37 +0100

    9. feladat - Absztrakció felhasználása a gyárakban

--- Gieda_Bianka_FYZINS_gyak8/Gieda_Bianka_FYZINS_gyak8/Entities/CarFactory.cs
+++ Gieda_Bianka_FYZINS_gyak8/Gieda_Bianka_FYZINS_gyak8/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace Gieda_Bianka_FYZINS_gyak8.Entities
 {
-    class BallFactory : IToyFactory
+    class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
